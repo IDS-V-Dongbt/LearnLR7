daaa1108e087a4907a138204610e59745fb58b49
dongbt <dongbt@vn.ids.jp>
2021-05-12 13:39:54 +0700

    CRUD

---
+++ app/models/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'product_name',
+        'price',
+        'slug',
+        'status',
+        'category_id'
+    ];
+
+    public function product_category()
+    {
+        return $this->belongsTo('App\models\ProductCategory');
+    }
+
+    public function product_images()
+    {
+        return $this->hasMany('App\models\ProductImage');
+    }
+}
