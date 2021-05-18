ab3b6f636752b9fa461f650a508466cfca19c642
dongbt <dongbt@vn.ids.jp>
2021-05-18 16:06:50 +0700

    custom dashboard

---
+++ resources/views/website/frontend_Hyas/footer.blade.php
@@ -0,0 +1,20 @@
+<div class="footer">
+    <div class="footer_left">
+        <div class="footer_left--text">
+            <a>個人情報保護法 <span></span></a>
+            <img src="{{asset('Hyas/frontend_login/img/icon_sent.PNG')}}" alt="">
+        </div>
+        <div class="footer_left--text">
+            <a>運営会社<span class="nth_2"></span></a>
+            <img src="{{asset('Hyas/frontend_login/img/icon_sent.PNG')}}" alt="">
+        </div>
+    </div>
+    <div class="footer_right">
+        <div class="footer_right--text">
+            <a>©2021 </a>
+        </div>
+        <div class="footer_left--text footer_left--text1">
+            <a>株式会社家価値サポート</a>
+        </div>
+    </div>
+</div>
--- resources/views/website/frontend_Hyas/login.blade.php
+++ resources/views/website/frontend_Hyas/login.blade.php
@@ -1,20 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('Hyas/frontend_login/css/main.css')}}" />   <!--Add this-->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet">  <!--Add this-->
-    
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">  <!--Add this-->
-
-    <title>Login Hyas</title>
-
-</head>
-
+@include('website.frontend_Hyas.head')
 <body>
     <div class="login">
         <div class="login__logo-box">
@@ -57,26 +44,7 @@
         </div>
       
     </div>
-    <div class="footer">
-        <div class="footer_left">
-            <div class="footer_left--text">
-                <a>個人情報保護法 <span></span></a>
-                <img src="{{asset('Hyas/frontend_login/img/icon_sent.PNG')}}" alt="">
-            </div>
-            <div class="footer_left--text">
-                <a>運営会社<span class="nth_2"></span></a>
-                <img src="{{asset('Hyas/frontend_login/img/icon_sent.PNG')}}" alt="">
-            </div>
-        </div>
-        <div class="footer_right">
-            <div class="footer_right--text">
-                <a>©2021 </a>
-            </div>
-            <div class="footer_left--text">
-                <a>株式会社家価値サポート</a>
-            </div>
-        </div>
-    </div>
+    @include('website.frontend_Hyas.footer')
 </body>
 
 </html>
